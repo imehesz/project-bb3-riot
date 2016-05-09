ba1620ff6669237e61256d2e389afc91155969a4
Imre Mehesz <imehesz@gmail.com>
2016-05-09 16:38:13 +0000

    Added simple search feature

--- js/books-page.js
+++ js/books-page.js
@@ -15,10 +15,25 @@
       MHX.Service.getBookList({
         successCb: (data) => {
           this.riotScope.update({
-            books: data
+            books: data,
+            filteredBooks: data
           });          
         }
       });
+      
+      this.riotScope.filterBooks = (text) => {
+        if (this.riotScope.books && this.riotScope.books.length) {
+          var tmpBooks = this.riotScope.books.filter(function(el){
+            if (el && el.shortHeader.toLowerCase().indexOf(text.toLowerCase()) > -1) {
+              return true;
+            }
+          });
+          
+          this.riotScope.update({
+            filteredBooks: tmpBooks
+          });
+        }
+      }
     }
   }
 
---
+++ js/search-form.js
@@ -0,0 +1,27 @@
+/* global MHX */
+/* global riot */
+/* global _t */
+
+(function(){
+  "use strict";
+
+  class SearchFormTag extends MHX.Tag {
+    constructor(riotScope) {
+      super(riotScope);
+    }
+    
+    onMount() {
+      super.onMount();
+      
+      this.riotScope.textChanged = (e) => {
+        if (this.riotScope.opts.changecb && typeof this.riotScope.opts.changecb) {
+          this.riotScope.opts.changecb(e.target.value);
+        } else {
+          console.log("YUP! changed", e.target.value);
+        }
+      }
+    }
+  }
+  
+  window.MHX.SearchFormTag = SearchFormTag;
+})();
